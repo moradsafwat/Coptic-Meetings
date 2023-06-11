73efed1b5ed203d7c58113afc6c912f931063f59
Morad_Safwat <morad.safwat@yahoo.com>
2023-06-11 21:42:15 +0300

    coptic Meeting Init Commit

---
+++ src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Morad Safwat',
+  email: 'morad.safwat@yahoo.com',
+  photoURL: '/assets/images/avatars/avatar_default.jpg',
+};
+
+export default account;
